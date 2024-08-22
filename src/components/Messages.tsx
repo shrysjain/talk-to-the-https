0ba782d72dc93af9392814d6b675b1b6f6cd6736
Shreyas Jain <shrys.jain@gmail.com>
2024-08-22 01:10:24 -0400

    landing page

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,5 +1,6 @@
 import { type Message as TMessage } from "ai/react";
 import { Message } from "./Message";
+import { MessageSquare } from "lucide-react";
 
 interface MessageProps {
   messages: TMessage[];
@@ -8,7 +9,7 @@ interface MessageProps {
 export const Messages = ({ messages }: MessageProps) => {
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7re)] flex-1 flex-col overflow-y-auto">
-      {messages ? (
+      {messages.length ? (
         messages.map((message, index) => (
           <Message
             key={index}
@@ -17,7 +18,16 @@ export const Messages = ({ messages }: MessageProps) => {
           />
         ))
       ) : (
-        <div></div>
+        <div className="flex-1 flex flex-col items-center justify-center gap-2 m-10">
+          <MessageSquare className="size-8 text-blue-500" />
+          <h3 className="font-semibold text-xl text-white">
+            Connected to website
+          </h3>
+          <p className="text-zinc-400 text-sm">
+            Ask a question, discuss the content, or just have a friendly
+            conversation with the bot.
+          </p>
+        </div>
       )}
     </div>
   );
